1029b912d60f76d0c970f64de32b6c6de348e546
Björn Schießle <bjoern@schiessle.org>
2015-12-18 15:04:05 +0100

    make sure that we first display the numeric statistics and then the charts

--- templates/part.content.php
+++ templates/part.content.php
@@ -33,18 +33,12 @@
 	</div>
 
 	<?php foreach ($_['categories'] as $category => $data) { ?>
+				
 		<?php if ($category !== 'stats') { ?>
 			<div class="section section-stats" id="<?php p('survey' . ucfirst($category)); ?>">
 			<h1><?php p(ucfirst($category));?></h1>
 			<?php foreach($data as $key => $value) { ?>
-
-				<?php if ($value['presentation'] === \OCA\PopularityContestServer\EvaluateStatistics::PRESENTATION_TYPE_DIAGRAM) {?>
-					<div class="chart">
-						<h2><?php p(($value['description']));?></h2>
-						<canvas id="<?php p($category . $key . 'Chart');?>" width="400" height="300"></canvas>
-					</div>
-				<?php }
-				if ($value['presentation'] === \OCA\PopularityContestServer\EvaluateStatistics::PRESENTATION_TYPE_NUMERICAL_EVALUATION) {?>
+				<?php if ($value['presentation'] === \OCA\PopularityContestServer\EvaluateStatistics::PRESENTATION_TYPE_NUMERICAL_EVALUATION) {?>
 					<h2><?php p(($value['description']));?></h2>
 					<p id="<?php p($category . $key . 'NumericMax');?>"><?php p($value['description']); ?> (largest installation): <span></span></p>
 					<br />
@@ -54,6 +48,18 @@
 					<br />
 				<?php } ?>
 			<?php } ?>
+
+
+				<?php foreach($data as $key => $value) { ?>
+
+					<?php if ($value['presentation'] === \OCA\PopularityContestServer\EvaluateStatistics::PRESENTATION_TYPE_DIAGRAM) {?>
+						<div class="chart">
+							<h2><?php p(($value['description']));?></h2>
+							<canvas id="<?php p($category . $key . 'Chart');?>" width="400" height="300"></canvas>
+						</div>
+					<?php }
+				} ?>
+
 			</div>
 		<?php } ?>
 	<?php } ?>
