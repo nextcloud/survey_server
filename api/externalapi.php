9e47dba9b72981e7e7f925a996a4950ae21a40c5
Bjoern Schiessle <bjoern@schiessle.org>
2016-07-08 14:39:16 +0200

    update license

--- api/externalapi.php
+++ api/externalapi.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @author Björn Schießle <schiessle@owncloud.com>
+ * @author Björn Schießle <bjoern@schiessle.org>
  *
- * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
  * @license AGPL-3.0
  *
  * This code is free software: you can redistribute it and/or modify
